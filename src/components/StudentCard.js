457067192074ad517295947bd0ce75a54a1adfd1
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-04-24 14:27:19 -0500

    stash

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -14,29 +14,17 @@ const StudentCard = ({
   handleClick
 }) => (
   <Card body>
-<<<<<<< HEAD
-    <CardTitle tag="h5">{name}</CardTitle>
-    <CardText>Grade: {grade}</CardText>
-    <CardText>Teacher: {teacher}</CardText>
-    <Button onClick={handleClick}>Print Student</Button>
-=======
       <CardTitle tag="h5">{name}</CardTitle>
       <CardText>Grade: {grade}</CardText>
       <CardText>Teacher: {teacher}</CardText>
       {handleClick ? <Button onClick={handleClick}>Print Student</Button> : ''}
->>>>>>> 7245a86b59dac7507c6791802ae0cb72715dfd54
   </Card>
 );
 
 StudentCard.propTypes = {
   name: PropTypes.string.isRequired,
-<<<<<<< HEAD
-  grade: PropTypes.number.isRequired,
-  teacher: PropTypes.string.isRequired,
-=======
   teacher: PropTypes.string.isRequired,
   grade: PropTypes.number.isRequired,
->>>>>>> 7245a86b59dac7507c6791802ae0cb72715dfd54
   handleClick: PropTypes.func
 };
 
