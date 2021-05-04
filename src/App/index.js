5da7463deaa5f672edebc94ee6a7d257a1274bf1
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-05-03 19:03:55 -0500

    added auth and login

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import NavBar from '../components/NavBar';
 import { getStudents } from '../helpers/data/studentData';
 import Routes from '../helpers/Routes';
 
 function App() {
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     getStudents().then(setStudents);
   }, []);
 
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        // something to happen
+        const userInfoObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        setUser(userInfoObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <NavBar user={user} />
       <Routes
         students={students}
         setStudents={setStudents}
